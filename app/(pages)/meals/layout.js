19464fe3d95be4ce092119755cd66c0f80ce45f5
ozgeonline <nisroc.414@hotmail.com>
2026-04-08 23:27:20 +0300

    implement meals data access layer and initial page layout structure

--- app/(pages)/meals/layout.js
+++ app/(pages)/meals/layout.js
@@ -2,7 +2,7 @@ import styles from "./layout.module.css";
 
 export default function MealsLayout({ children }) {
   return (
-    <div className={styles.container + ' ' + "mainBackground"}>
+    <div className={`${styles.container} mainBackground`}>
       <div className="containerTopNavbarColor" />
       {children}
     </div>
